6e2a24bba647ab1898c1dcf296d7ee07953ae5d4
PvsKrishnaMohan <pvskrishnamohan479@gmail.com>
2025-02-24 22:32:39 +0530

    closure 1

---
+++ jsQuestions/closures.js
@@ -0,0 +1,14 @@
+// closures
+
+// function returning along with its lexical scope bundled together is closure
+function OuterFun() {
+    let z = 9;
+    function inner() {
+        console.log(z);
+    }
+    return inner;
+}
+
+const res = OuterFun(); // Call OuterFun to get the inner function
+console.log(res);
+res()
