af2b4b5bedc32badce5db3dea11c67f6e01af429
PvsKrishnaMohan <pvskrishnamohan479@gmail.com>
2025-04-13 10:00:18 +0530

    q2

--- jsQuestions/C_ValueToString.js
+++ jsQuestions/C_ValueToString.js
@@ -7,3 +7,7 @@ console.log(res)
 
 // Create a list of items that can be reordered via drag-and-drop using HTML Drag API or libraries like react-beautiful-dnd.
 // Time-Saver Tip: Provide starter boilerplate or just ask for logic functions (like onDragEnd()).
+
+const arr = [ {id:1}, {id:2}, {id:3} ]
+const re1s = arr.find((item) => item.id===2);
+console.log(re1s)
